25c2ba7ace10c68f2aa35ecaea2e6bc55c2f5a2c
deejayy <deejayy@deejayy.hu>
2024-07-12 17:35:56 +0200

    PoC

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-console.log("linkedin noise remover");
+(() => {
+  let observer = new MutationObserver(() => {
+    let items = document.querySelectorAll(
+      ".feed-shared-update-v3, .feed-shared-update-v2"
+    );
+    items.forEach((item) => {
+      if (
+        item.outerHTML.match(
+          /((likes|celebrates|loves|supports) this|finds this (funny|insightful)|anniversary|Just finished the course|s job update)/
+        )
+      ) {
+        item.remove();
+      }
+    });
+  });
+  observer.observe(document.documentElement, {
+    childList: true,
+    subtree: true,
+  });
+})();
